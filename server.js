4fab7cb61d8ee259774557ccf480587c94f1e5a3
varshinisensak <80758131+varshinisensak@users.noreply.github.com>
2021-05-26 06:36:07 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize')
+var sequelize = require('sequelize')
+var connection = new Sequelize('rabbit', 'root', 'Varshini1998@', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+var Article = connection.define('article', {
+    title: sequelize.STRING,
+    body: sequelize.TEXT
+})
+
+var Article2 = connection.define('article2', {
+    section: sequelize.STRING,
+    ref: sequelize.STRING
+})
+
+// const getData = async () => {
+//     const data = await
+// }
+
+Article.hasOne(Article2, {
+    foreignKey: "title"
+});
+Article2.belongsTo(Article);
+
+connection.sync()
