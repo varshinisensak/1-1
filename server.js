1bcbea2cb36fefc65882eb407138983839406b85
varshinisensak <80758131+varshinisensak@users.noreply.github.com>
2021-05-26 23:25:44 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const { Sequelize } = require('sequelize')
+
 var connection = new Sequelize('rabbit', 'root', 'Varshini1998@', {
     host: 'localhost',
     dialect: 'mysql'
 });
 var Article = connection.define('article', {
-    title: sequelize.STRING,
-    body: sequelize.TEXT
+    title: Sequelize.STRING,
+    body: Sequelize.TEXT
 })
 
 var Article2 = connection.define('article2', {
-    section: sequelize.STRING,
-    ref: sequelize.STRING
+    section: Sequelize.STRING,
+    ref: Sequelize.STRING
 })
 
 Article.hasOne(Article2, {
